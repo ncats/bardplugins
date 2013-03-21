8c388ce3f48d67cd116e868fbf1fda9cc0342d99
Rajarshi Guha <rajarshi.guha@gmail.com>
2013-03-21 09:04:02 -0400

    Updated to expose calculated results in JSON format

--- smartcyp/src/gov/nih/ncgc/bardplugin/SMARTCyp.java
+++ smartcyp/src/gov/nih/ncgc/bardplugin/SMARTCyp.java
@@ -1,45 +1,42 @@
 package gov.nih.ncgc.bardplugin;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sun.jersey.api.NotFoundException;
 import gov.nih.ncgc.bard.entity.Compound;
 import gov.nih.ncgc.bard.entity.Substance;
 import gov.nih.ncgc.bard.plugin.IPlugin;
 import gov.nih.ncgc.bard.plugin.PluginManifest;
 import gov.nih.ncgc.bard.tools.DBUtils;
-import org.openscience.cdk.CDKConstants;
-import org.openscience.cdk.DefaultChemObjectBuilder;
-import org.openscience.cdk.MoleculeSet;
+import org.openscience.cdk.*;
 import org.openscience.cdk.aromaticity.CDKHueckelAromaticityDetector;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.geometry.GeometryTools;
+import org.openscience.cdk.geometry.Projector;
 import org.openscience.cdk.graph.ConnectivityChecker;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IMolecule;
 import org.openscience.cdk.interfaces.IMoleculeSet;
-import org.openscience.cdk.renderer.AtomContainerRenderer;
-import org.openscience.cdk.renderer.font.AWTFontManager;
-import org.openscience.cdk.renderer.generators.BasicAtomGenerator;
-import org.openscience.cdk.renderer.generators.BasicSceneGenerator;
-import org.openscience.cdk.renderer.generators.IGenerator;
-import org.openscience.cdk.renderer.generators.RingGenerator;
-import org.openscience.cdk.renderer.visitor.AWTDrawVisitor;
+import org.openscience.cdk.io.MDLV2000Writer;
+import org.openscience.cdk.io.listener.PropertiesListener;
+import org.openscience.cdk.layout.StructureDiagramGenerator;
 import org.openscience.cdk.smiles.DeduceBondSystemTool;
 import org.openscience.cdk.smiles.SmilesParser;
 import org.openscience.cdk.tools.CDKHydrogenAdder;
 import org.openscience.cdk.tools.manipulator.AtomContainerManipulator;
-import smartcyp.*;
+import smartcyp.MoleculeKU;
+import smartcyp.SMARTSnEnergiesTable;
+import smartcyp.WriteResultsAsChemDoodleHTML;
 
-import javax.imageio.ImageIO;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.awt.image.RenderedImage;
 import java.io.*;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 /**
  * @author Rajarshi Guha
@@ -80,6 +77,75 @@ public class SMARTCyp implements IPlugin {
             throw new WebApplicationException(e, 500);
         }
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/{smiles}")
+    public Response getSmartCypResults(@PathParam("smiles") String smiles) {
+        try {
+            JsonNode data = getResults(smiles);
+            ObjectMapper mapper = new ObjectMapper();
+            return Response.ok(mapper.writeValueAsString(data)).type(MediaType.APPLICATION_JSON).build();
+        } catch (CDKException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (CloneNotSupportedException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (FileNotFoundException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (IOException e) {
+            throw new WebApplicationException(e, 500);
+        }
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/cid/{cid}")
+    public Response getSmartCypResultsByCid(@PathParam("cid") Long cid) {
+        try {
+            DBUtils db = new DBUtils();
+            List<Compound> cmpds = db.getCompoundsByCid(cid);
+            if (cmpds == null || cmpds.size() == 0) throw new NotFoundException();
+            db.closeConnection();
+            Compound cmpd = cmpds.get(0);
+            JsonNode data = getResults(cmpd.getSmiles());
+            ObjectMapper mapper = new ObjectMapper();
+            return Response.ok(mapper.writeValueAsString(data)).type(MediaType.APPLICATION_JSON).build();
+        } catch (CDKException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (CloneNotSupportedException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (FileNotFoundException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (IOException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (SQLException e) {
+            throw new WebApplicationException(e, 500);
+        }
+    }
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("/sid/{sid}")
+    public Response getSmartCypResultsBySid(@PathParam("sid") Long sid) {
+        try {
+            DBUtils db = new DBUtils();
+            Substance subst = db.getSubstanceBySid(sid);
+            if (subst == null) throw new NotFoundException();
+            db.closeConnection();
+            JsonNode data = getResults(subst.getSmiles());
+            ObjectMapper mapper = new ObjectMapper();
+            return Response.ok(mapper.writeValueAsString(data)).type(MediaType.APPLICATION_JSON).build();
+        } catch (CDKException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (CloneNotSupportedException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (FileNotFoundException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (IOException e) {
+            throw new WebApplicationException(e, 500);
+        } catch (SQLException e) {
+            throw new WebApplicationException(e, 500);
+        }
+    }
+
 
     @GET
     @Produces(MediaType.TEXT_HTML)
@@ -131,6 +197,129 @@ public class SMARTCyp implements IPlugin {
     }
 
 
+    private JsonNode getResults(String smiles) throws CDKException, CloneNotSupportedException {
+        ObjectMapper mapper = new ObjectMapper();
+
+        IAtomContainer iAtomContainer = process(smiles);
+
+        TreeSet<Atom> sortedAtomsTreeSet = ((MoleculeKU) iAtomContainer).getAtomsSortedByEnA();
+        Iterator<Atom> sortedAtomsTreeSetIterator = sortedAtomsTreeSet.iterator();
+        IAtom atom;
+
+        ArrayNode anode3a4 = mapper.createArrayNode();
+        while (sortedAtomsTreeSetIterator.hasNext()) {
+            atom = sortedAtomsTreeSetIterator.next();
+            int rank = MoleculeKU.SMARTCYP_PROPERTY.Ranking.get(atom).intValue();
+            String id = atom.getSymbol() + "." + atom.getID();
+            Number score = MoleculeKU.SMARTCYP_PROPERTY.Score.get(atom);
+            Number energy = MoleculeKU.SMARTCYP_PROPERTY.Energy.get(atom);
+            Number acc = MoleculeKU.SMARTCYP_PROPERTY.Accessibility.get(atom);
+            Number sasa2d = MoleculeKU.SMARTCYP_PROPERTY.SASA2D.get(atom);
+
+            ObjectNode node = mapper.createObjectNode();
+            node.put("rank", rank);
+            node.put("id", id);
+            node.put("score", score.floatValue());
+            node.put("energy", energy.floatValue());
+            node.put("acc", acc.floatValue());
+            node.put("sasa2d", sasa2d.floatValue());
+
+            anode3a4.add(node);
+        }
+
+        sortedAtomsTreeSet = ((MoleculeKU) iAtomContainer).getAtomsSortedByEnA2D6();
+        sortedAtomsTreeSetIterator = sortedAtomsTreeSet.iterator();
+        ArrayNode anode2d6 = mapper.createArrayNode();
+        while (sortedAtomsTreeSetIterator.hasNext()) {
+            atom = sortedAtomsTreeSetIterator.next();
+            int rank = MoleculeKU.SMARTCYP_PROPERTY.Ranking2D6.get(atom).intValue();
+            String id = atom.getSymbol() + "." + atom.getID();
+            Number score = MoleculeKU.SMARTCYP_PROPERTY.Score2D6.get(atom);
+            Number energy = MoleculeKU.SMARTCYP_PROPERTY.Energy.get(atom);
+            Number acc = MoleculeKU.SMARTCYP_PROPERTY.Accessibility.get(atom);
+            Number sasa2d = MoleculeKU.SMARTCYP_PROPERTY.SASA2D.get(atom);
+
+            ObjectNode node = mapper.createObjectNode();
+            node.put("rank", rank);
+            node.put("id", id);
+            node.put("score", score.floatValue());
+            node.put("energy", energy.floatValue());
+            node.put("acc", acc.floatValue());
+            node.put("sasa2d", sasa2d.floatValue());
+
+            anode2d6.add(node);
+        }
+
+        sortedAtomsTreeSet = ((MoleculeKU) iAtomContainer).getAtomsSortedByEnA2C9();
+        sortedAtomsTreeSetIterator = sortedAtomsTreeSet.iterator();
+        ArrayNode anode2c9 = mapper.createArrayNode();
+        while (sortedAtomsTreeSetIterator.hasNext()) {
+            atom = sortedAtomsTreeSetIterator.next();
+            int rank = MoleculeKU.SMARTCYP_PROPERTY.Ranking2C9.get(atom).intValue();
+            String id = atom.getSymbol() + "." + atom.getID();
+            Number score = MoleculeKU.SMARTCYP_PROPERTY.Score2C9.get(atom);
+            Number energy = MoleculeKU.SMARTCYP_PROPERTY.Energy.get(atom);
+            Number acc = MoleculeKU.SMARTCYP_PROPERTY.Accessibility.get(atom);
+            Number sasa2d = MoleculeKU.SMARTCYP_PROPERTY.SASA2D.get(atom);
+
+            ObjectNode node = mapper.createObjectNode();
+            node.put("rank", rank);
+            node.put("id", id);
+            node.put("score", score.floatValue());
+            node.put("energy", energy.floatValue());
+            node.put("acc", acc.floatValue());
+            node.put("sasa2d", sasa2d.floatValue());
+
+            anode2c9.add(node);
+        }
+
+        // finally get the molecule with coords and atom ids
+        iAtomContainer = generate2Dcoordinates((MoleculeKU) iAtomContainer);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        MDLV2000Writer writer = new MDLV2000Writer(baos);
+        Properties customSettings = new Properties();
+        customSettings.setProperty("ForceWriteAs2DCoordinates", "true");
+        PropertiesListener listener = new PropertiesListener(customSettings);
+        writer.addChemObjectIOListener(listener);
+        try {
+            writer.write(iAtomContainer);
+        } catch (CDKException e) {
+        }
+        String molstr = baos.toString();
+//        molstr = molstr.replace("\\n", "\\\\n");
+//        String[] mollines = molstr.split("\\r?\\n");
+
+
+        ObjectNode root = mapper.createObjectNode();
+        root.put("structure", molstr);
+        root.put("3A4", anode3a4);
+        root.put("2D6", anode2d6);
+        root.put("2C9", anode2c9);
+
+        return root;
+    }
+
+    MoleculeKU generate2Dcoordinates(MoleculeKU iAtomContainer) {
+        final StructureDiagramGenerator structureDiagramGenerator = new StructureDiagramGenerator();
+        if (GeometryTools.has2DCoordinates(iAtomContainer)) {
+            return iAtomContainer; // already has 2D coordinates.
+        } else {
+            final AtomContainer iAtomContainer2d = new AtomContainer();
+            synchronized (structureDiagramGenerator) {
+                structureDiagramGenerator.setMolecule(iAtomContainer, true);
+                structureDiagramGenerator.setUseTemplates(true);
+                try {
+                    structureDiagramGenerator.generateCoordinates();
+                    iAtomContainer = (MoleculeKU) structureDiagramGenerator.getMolecule();
+                } catch (final Exception e) {
+                    Projector.project2D(iAtomContainer);
+                }
+            }
+            if (GeometryTools.has2DCoordinates(iAtomContainer)) return iAtomContainer;
+            else return null;
+        }
+    }
+
     String getHtmlSummaryPage(String smiles) throws CDKException, CloneNotSupportedException, IOException {
         UUID uuid = UUID.randomUUID();
         String fname = "/tmp/" + uuid.toString();
@@ -148,51 +337,6 @@ public class SMARTCyp implements IPlugin {
         return sb.toString();
     }
 
-
-    @GET
-    @Produces("image/png")
-    @Path("/{smiles}/image")
-    public Response getSmartCypImage(@PathParam("smiles") String smiles) {
-        try {
-            int WIDTH = 800;
-            int HEIGHT = 400;
-            Rectangle drawArea = new Rectangle(WIDTH, HEIGHT);
-            Image image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-
-            IAtomContainer iAtomContainer = process(smiles);
-            GenerateImages genimg = new GenerateImages(null, null);
-            iAtomContainer = genimg.generate2Dcoordinates(iAtomContainer);
-
-            List<IGenerator<IAtomContainer>> generators = new ArrayList<IGenerator<IAtomContainer>>();
-//            generators.add(new BasicSceneGenerator());
-//            generators.add(new RingGenerator());
-//            generators.add(new BasicAtomGenerator());
-//            generators.add(new AtomNumberGenerator());
-
-            generators.add(new BasicSceneGenerator());
-            generators.add(new rankedlabelgenerator2C9());
-            generators.add(new RingGenerator());
-            generators.add(new BasicAtomGenerator());
-
-
-            AtomContainerRenderer renderer = new AtomContainerRenderer(generators, new AWTFontManager());
-            renderer.setup(iAtomContainer, drawArea);
-
-            Graphics2D g2 = (Graphics2D) image.getGraphics();
-            g2.fillRect(0, 0, WIDTH, HEIGHT);
-            renderer.paint(iAtomContainer, new AWTDrawVisitor(g2), drawArea, true);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write((RenderedImage) image, "PNG", baos);
-
-            return Response.ok(baos.toByteArray()).type("image/png").build();
-        } catch (CDKException e) {
-            throw new WebApplicationException(e, 500);
-        } catch (CloneNotSupportedException e) {
-            throw new WebApplicationException(e, 500);
-        } catch (IOException e) {
-            throw new WebApplicationException(e, 500);
-        }
-    }
 
     private MoleculeKU process(String smiles) throws CDKException, CloneNotSupportedException {
         SMARTSnEnergiesTable SMARTSnEnergiesTable = new SMARTSnEnergiesTable();
@@ -260,7 +404,7 @@ public class SMARTCyp implements IPlugin {
         pm.setMaintainerEmail("guhar@mail.nih.gov");
         pm.setTitle("SMARTCyp");
         pm.setDescription("SMARTCyp is a method for prediction of which sites in a molecule that are most liable to metabolism by Cytochrome P450. See http://www.farma.ku.dk/smartcyp/about.php for more details and the original code");
-        pm.setVersion("1.0");
+        pm.setVersion("1.1");
 
         PluginManifest.PluginResource res1 = new PluginManifest.PluginResource();
         res1.setPath("/{smiles}/summary");
@@ -280,7 +424,26 @@ public class SMARTCyp implements IPlugin {
         res3.setMethod("GET");
         res3.setArgs(new PluginManifest.PathArg[]{new PluginManifest.PathArg("sid", "number")});
 
-        pm.setResources(new PluginManifest.PluginResource[]{res1, res2, res3});
+        PluginManifest.PluginResource res4 = new PluginManifest.PluginResource();
+        res4.setPath("/{smiles}");
+        res4.setMimetype("application/json");
+        res4.setMethod("GET");
+        res4.setArgs(new PluginManifest.PathArg[]{new PluginManifest.PathArg("smiles", "string")});
+
+        PluginManifest.PluginResource res5 = new PluginManifest.PluginResource();
+        res5.setPath("/cid/{cid}");
+        res5.setMimetype("application/json");
+        res5.setMethod("GET");
+        res5.setArgs(new PluginManifest.PathArg[]{new PluginManifest.PathArg("cid", "number")});
+
+        PluginManifest.PluginResource res6 = new PluginManifest.PluginResource();
+        res6.setPath("/sid/{sid}");
+        res6.setMimetype("application/json");
+        res6.setMethod("GET");
+        res6.setArgs(new PluginManifest.PathArg[]{new PluginManifest.PathArg("sid", "number")});
+
+
+        pm.setResources(new PluginManifest.PluginResource[]{res1, res2, res3, res4, res5, res6});
 
         return pm.toJson();
     }
