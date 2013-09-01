480d6ba03f4d7383f6ac50caeee417550305e33b
Rajarshi Guha <rajarshi.guha@gmail.com>
2013-09-01 13:46:53 -0400

    Added two more resources (get classes in bulk and get accs for class)

--- protclass/src/gov/nih/ncgc/bardplugin/ProteinClass.java
+++ protclass/src/gov/nih/ncgc/bardplugin/ProteinClass.java
@@ -4,12 +4,17 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.sun.jersey.api.NotFoundException;
+import gov.nih.ncgc.bard.entity.ProteinTarget;
 import gov.nih.ncgc.bard.entity.TargetClassification;
 import gov.nih.ncgc.bard.plugin.IPlugin;
 import gov.nih.ncgc.bard.plugin.PluginManifest;
 import gov.nih.ncgc.bard.tools.DBUtils;
+import gov.nih.ncgc.bard.tools.Util;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -63,6 +68,50 @@ public class ProteinClass implements IPlugin {
         return Response.ok(json).type(MediaType.APPLICATION_JSON_TYPE).build();
     }
 
+    @POST
+    @Path("/{source}")
+    @Consumes("application/x-www-form-urlencoded")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getClassificationsForAccessions(@PathParam("source") String source,
+                                                    @FormParam("ids") String accs)
+            throws SQLException, IOException {
+
+        DBUtils db = new DBUtils();
+        String[] laccs = accs.split(",");
+
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode node = mapper.createObjectNode();
+
+        for (String acc : laccs) {
+            List<TargetClassification> classes = null;
+            if (source.toLowerCase().equals("panther")) {
+                classes = db.getPantherClassesForAccession(acc.trim());
+            }
+            JsonNode classNodes = mapper.valueToTree(classes);
+            node.put(acc, classNodes);
+        }
+        db.closeConnection();
+        String json = mapper.writeValueAsString(node);
+        return Response.ok(json).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
+    @GET
+    @Path("/classification/{source}/{clsid}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAccessionsForClassification(@PathParam("source") String source,
+                                                   @PathParam("clsid") String clsid) 
+            throws SQLException, IOException {
+        List<ProteinTarget> targets = null;
+        DBUtils db = new DBUtils();
+        if (source.toLowerCase().equals("panther")) {
+            targets = db.getProteinTargetsForPantherClassification(clsid);
+        }
+        db.closeConnection();
+        if (targets == null)
+            throw new NotFoundException("No protein targets for " + clsid + " in the " + source + " hierarchy");
+        return Response.ok(Util.toJson(targets)).type(MediaType.APPLICATION_JSON_TYPE).build();
+    }
+
 
     @GET
     @Path("/_manifest")
@@ -86,7 +135,25 @@ public class ProteinClass implements IPlugin {
                 new PluginManifest.PathArg("id", "string", "path"),
         });
 
-        pm.setResources(new PluginManifest.PluginResource[]{res1});
+        PluginManifest.PluginResource res2 = new PluginManifest.PluginResource();
+        res2.setPath("/{source}");
+        res2.setMimetype("application/json");
+        res2.setMethod("POST");
+        res2.setArgs(new PluginManifest.PathArg[]{
+                new PluginManifest.PathArg("source", "string", "path"),
+                new PluginManifest.PathArg("ids", "string", "form"),
+        });
+
+        PluginManifest.PluginResource res3 = new PluginManifest.PluginResource();
+        res3.setPath("/{source}");
+        res3.setMimetype("application/json");
+        res3.setMethod("POST");
+        res3.setArgs(new PluginManifest.PathArg[]{
+                new PluginManifest.PathArg("source", "string", "path"),
+                new PluginManifest.PathArg("clsid", "string", "path"),
+        });
+
+        pm.setResources(new PluginManifest.PluginResource[]{res1, res2, res3});
 
         return pm.toJson();
     }
