3938ebaa818ea363e097a92f22c1441fd44b72a1
Rajarshi Guha <rajarshi.guha@gmail.com>
2013-09-24 10:53:29 -0400

    Added example plugin to highlight per-request initialization versus servlet load time initialization

---
+++ initdemo/src/gov/nih/ncgc/bardplugin/InitDemo.java
@@ -0,0 +1,77 @@
+package gov.nih.ncgc.bardplugin;
+
+import gov.nih.ncgc.bard.plugin.IPlugin;
+import gov.nih.ncgc.bard.plugin.PluginManifest;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Rajarshi Guha
+ */
+@Path("/initdemo")
+public class InitDemo implements IPlugin, ServletContextListener {
+    private static int servletInitCounter = 0;
+    private static int ctorInitCounter = 0;
+
+    public InitDemo() {
+        System.out.println("InitDemo ctor called");
+        ctorInitCounter++;
+    }
+
+    @GET
+    @Produces("text/plain")
+    @Path("/_info")
+    public String getDescription() {
+        return "Demo plugin";
+    }
+
+
+    @GET
+    @Produces("text/plain")
+    @Path("/res")
+    public Response get() {
+        return Response.ok("Calling this URL multiple times should show ctorInitCounter increasing, but" +
+                " servletInitCount remaining at 1.\n" +
+                "servletInitCounter " + servletInitCounter + " ctorInitCounter " + ctorInitCounter,
+                MediaType.TEXT_PLAIN_TYPE).build();
+    }
+
+
+    @GET
+    @Path("/_manifest")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getManifest() {
+        PluginManifest pm = new PluginManifest();
+        pm.setAuthor("Rajarshi Guha");
+        pm.setAuthorEmail("guhar@mail.nih.gov");
+        pm.setMaintainer("Rajarshi Guha");
+        pm.setMaintainerEmail("guhar@mail.nih.gov");
+        pm.setTitle("InitDemo");
+        pm.setDescription("Test plugin initialization");
+        pm.setVersion("1.0");
+
+        PluginManifest.PluginResource res1 = new PluginManifest.PluginResource();
+        res1.setPath("/res");
+        res1.setMimetype("text/plain");
+        res1.setMethod("GET");
+
+        pm.setResources(new PluginManifest.PluginResource[]{res1});
+
+        return pm.toJson();
+    }
+
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("contextInitialized() was called");
+        servletInitCounter++;
+    }
+
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("contextDestroyed() called");
+    }
+}
